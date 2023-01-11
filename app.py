911829c2d45c96776a14933f98b3d908ae4b458a
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-01-11 15:27:43 +0300

    Adding a file shortener

--- app.py
+++ app.py
@@ -1,11 +1,38 @@
-from flask import Flask
+from flask import Flask,render_template,request,redirect,url_for,flash
+import json
+import os.path
+from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
+app.secret_key = 'h234567843rerfeddstt3u3uegfdftgdyey'
 
 @app.route('/')
 @app.route('/home')
 def home():
-    return 'hello flask with kashee!'
-@app.route('/about')
-def about_page():
-    return 'This is a url shortener'
+    return render_template('home.html')
+@app.route('/your-url', methods=['GET','POST'])
+def your_url():
+    if request.method == 'POST':
+       urls={}
+       
+       if os.path.exists('urls.json'):
+           with open('urls.json') as url_file:
+               urls = json.load(url_file)
+       if request.form['code'] in urls.keys():
+           flash('That short name is already taken. Please input another.')
+           return redirect(url_for('home')) 
+       
+       if 'url' in request.form.keys():
+           urls[request.form['code']] = {'url':request.form['url']}
+       else:
+           f = request.files['file']
+           full_name = request.form['code'] + secure_filename(f.filename)
+           f.save('/home/muchirinjeri/urlshortener/' + full_name)
+           urls[request.form['code']] = {'file': full_name}
+            
+       with open('urls.json', 'w') as urls_file:
+            json.dump(urls, urls_file)
+       return render_template('your_url.html', code=request.form['code'])
+    else:
+       return redirect(url_for('home'))
+   
