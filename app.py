e0ba20f87b33cbafed095d41a1f24db95d5c94d8
Njeri Muchiri <muchirinjeri3210@gmail.com>
2023-01-11 16:40:23 +0300

    Making session cookies

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask,render_template,request,redirect,url_for,flash,abort
+from flask import Flask,render_template,request,redirect,url_for,flash,abort,session
 import json
 import os.path
 from werkzeug.utils import secure_filename
@@ -9,7 +9,8 @@ app.secret_key = 'h234567843rerfeddstt3u3uegfdftgdyey'
 @app.route('/')
 @app.route('/home')
 def home():
-    return render_template('home.html')
+    return render_template('home.html', codes=session.keys())
+    
 @app.route('/your-url', methods=['GET','POST'])
 def your_url():
     if request.method == 'POST':
@@ -32,6 +33,7 @@ def your_url():
             
        with open('urls.json', 'w') as urls_file:
             json.dump(urls, urls_file)
+            session[request.form['code']] = True
        return render_template('your_url.html', code=request.form['code'])
     else:
        return redirect(url_for('home'))
@@ -41,6 +43,7 @@ def redirect_to_url(code):
     if os.path.exists('urls.json'):
         with open('urls.json') as url_file:
             urls = json.load(url_file)
+        
             if code in urls.keys():
                 if 'url' in urls[code].keys():
                     return redirect(urls[code]['url'])
